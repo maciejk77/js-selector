dc9a8c1fd10ae435a4749d706e57e12288772dd8
Maciej Kurek <maciejk77@gmail.com>
2015-11-22 12:15:25 +0000

    3/7 tests passing

--- Answer.js
+++ Answer.js
@@ -1,9 +1,26 @@
 var $ = function (selector) {
   var elements = [];
+  var selectors = selector.split(/(?=\.)|(?=#)/);
+  var nodes = getElements(selectors[0]);
 
-  ////////////////////
-  // Your code here //
-  ////////////////////
+  if(selectors.length === 1) {
+    return nodes;
+  } else {
+    console.log('selectors length is longer than one: ' + selector);
+  }
 
   return elements;
+
+function getElements(selector) {
+  var result = [];
+  if(selector.charAt(0) === '#') {
+    result = document.getElementById(selector.substring(1));
+  } else if (selector.charAt(0) === '.') {
+    result = document.getElementsByClassName(selector.substring(1));
+  } else {
+    result = document.getElementsByTagName(selector);
+  }
+  return result
+}
+
 }
